c6de2af825ee662a88a1ea5aed7f9b73e7645ea0
mohammadmjalawneh <mohammadmjalawneh@users.github.com>
2021-01-05 23:34:58 +0200

    1st Assaiment

---
+++ firstone/resources/views/contactus.blade.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<link rel="icon" href="{{url(''.'img/favicon.png')}}">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Optional theme -->
+</head>
+<style type="text/css">
+	.site-footer
+	{
+		background-color:#26272b;
+		padding:45px 0 20px;
+		font-size:15px;
+		line-height:24px;
+		color:#737373;
+	}
+	.site-footer hr
+	{
+		border-top-color:#bbb;
+		opacity:0.5
+	}
+	.site-footer hr.small
+	{
+		margin:20px 0
+	}
+	.site-footer h6
+	{
+		color:#fff;
+		font-size:16px;
+		text-transform:uppercase;
+		margin-top:5px;
+		letter-spacing:2px
+	}
+	.site-footer a
+	{
+		color:#737373;
+	}
+	.site-footer a:hover
+	{
+		color:#3366cc;
+		text-decoration:none;
+	}
+	.footer-links
+	{
+		padding-left:0;
+		list-style:none
+	}
+	.footer-links li
+	{
+		display:block
+	}
+	.footer-links a
+	{
+		color:#737373
+	}
+	.footer-links a:active,.footer-links a:focus,.footer-links a:hover
+	{
+		color:#3366cc;
+		text-decoration:none;
+	}
+	.footer-links.inline li
+	{
+		display:inline-block
+	}
+	.site-footer .social-icons
+	{
+		text-align:right
+	}
+	.site-footer .social-icons a
+	{
+		width:40px;
+		height:40px;
+		line-height:40px;
+		margin-left:6px;
+		margin-right:0;
+		border-radius:100%;
+		background-color:#33353d
+	}
+	.copyright-text
+	{
+		margin:0
+	}
+	@media (max-width:991px)
+	{
+		.site-footer [class^=col-]
+		{
+			margin-bottom:30px
+		}
+	}
+	@media (max-width:767px)
+	{
+		.site-footer
+		{
+			padding-bottom:0
+		}
+		.site-footer .copyright-text,.site-footer .social-icons
+		{
+			text-align:center
+		}
+	}
+	.social-icons
+	{
+		padding-left:0;
+		margin-bottom:0;
+		list-style:none
+	}
+	.social-icons li
+	{
+		display:inline-block;
+		margin-bottom:4px
+	}
+	.social-icons li.title
+	{
+		margin-right:15px;
+		text-transform:uppercase;
+		color:#96a2b2;
+		font-weight:700;
+		font-size:13px
+	}
+	.social-icons a{
+		background-color:#eceeef;
+		color:#818a91;
+		font-size:16px;
+		display:inline-block;
+		line-height:44px;
+		width:44px;
+		height:44px;
+		text-align:center;
+		margin-right:8px;
+		border-radius:100%;
+		-webkit-transition:all .2s linear;
+		-o-transition:all .2s linear;
+		transition:all .2s linear
+	}
+	.social-icons a:active,.social-icons a:focus,.social-icons a:hover
+	{
+		color:#fff;
+		background-color:#29aafe
+	}
+	.social-icons.size-sm a
+	{
+		line-height:34px;
+		height:34px;
+		width:34px;
+		font-size:14px
+	}
+	.social-icons a.facebook:hover
+	{
+		background-color:#3b5998
+	}
+	.social-icons a.twitter:hover
+	{
+		background-color:#00aced
+	}
+	.social-icons a.linkedin:hover
+	{
+		background-color:#007bb6
+	}
+	.social-icons a.dribbble:hover
+	{
+		background-color:#ea4c89
+	}
+	@media (max-width:767px)
+	{
+		.social-icons li.title
+		{
+			display:block;
+			margin-right:0;
+			font-weight:600
+		}
+	}
+</style>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand pr-5" href="http://127.0.0.1:8000/"><i style="font-size: 2rem;" class="fa fa-twitter"></i></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="http://127.0.0.1:8000/">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="{{url('../ourteam')}}">Our Team</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="{{url('../contactus')}}">Contact us</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<div class="container">
+		<div class="row m-5">
+			<div class="col-6">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2482.888715146913!2d-0.144228184799362!3d51.51525761795049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761ad554c335c1%3A0xda2164b934c67c1a!2sOxford%20St%2C%20London%2C%20UK!5e0!3m2!1sen!2sjo!4v1609799294863!5m2!1sen!2sjo" width="400" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+			</div>
+			<div class="col-6">
+				<form>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Your Name</label>
+						<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email address</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Phone</label>
+						<input type="tel" class="form-control" id="exampleInputPassword1" placeholder="Enter Phone">
+					</div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<footer class="site-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-6">
+					<h6>About</h6>
+					<p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+				</div>
+
+
+				<div class="col-xs-6 col-md-3">
+					<h6>Quick Links</h6>
+					<ul class="footer-links">
+						<li><a href="http://127.0.0.1:8000/">Home</a></li>
+						<li><a href="http://127.0.0.1:8000/ourteam">Our Team</a></li>
+						<li><a href="http://127.0.0.1:8000/contactus">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+			<hr>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-sm-6 col-xs-12">
+					<p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+						<a href="#">Scanfcode</a>.
+					</p>
+				</div>
+
+				<div class="col-md-4 col-sm-6 col-xs-12">
+					<ul class="social-icons">
+						<li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+						<li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
